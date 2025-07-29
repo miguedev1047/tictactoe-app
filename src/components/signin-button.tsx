4bfbaade0e023905fa55d2482f2341f44f57515a
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-29 11:27:01 -0500

    FEAT: refactor signin button ⚒️

--- src/components/signin-button.tsx
+++ src/components/signin-button.tsx
@@ -1,19 +1,33 @@
 'use client'
 
+import React, { useTransition } from 'react'
 import { Button } from '@/components/ui/8bit/button'
 import { toast } from '@/components/ui/8bit/toast'
-import { useTransition } from 'react'
 import { authClient } from '@/lib/auth-client'
-import { useRouter } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
+import { cn } from '@/lib/utils'
 
-export function DiscordSignin() {
+export interface Props extends React.ComponentProps<typeof Button> {
+  provider: 'discord' | 'github' | 'google'
+}
+
+export function SigninButton({
+  children,
+  provider,
+  className,
+  ...props
+}: Props) {
   const router = useRouter()
+  const { roomId } = useParams<{ roomId: string }>()
   const [isPending, startTransition] = useTransition()
 
   const handleSignIn = () => {
+    const callbackURL = roomId ? `/invite/${roomId}` : '/home'
+
     startTransition(async () => {
       await authClient.signIn.social({
-        provider: 'discord',
+        provider: provider,
+        callbackURL,
         fetchOptions: {
           onSuccess: () => {
             toast('Sign in...')
@@ -32,8 +46,10 @@ export function DiscordSignin() {
     <Button
       disabled={isPending}
       onClick={handleSignIn}
+      className={cn(className)}
+      {...props}
     >
-      Sign in with discord
+      {children}
     </Button>
   )
 }

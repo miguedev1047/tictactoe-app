d7b53b167b46ef40e0fe743c83d59ce857496a72
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-29 11:26:01 -0500

    FEAT: import routers ⚙️

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,9 +1,17 @@
-import { createCallerFactory, createTRPCRouter, publicProcedure } from '@/server/api/trpc'
+import {
+  createCallerFactory,
+  createTRPCRouter,
+  publicProcedure,
+} from '@/server/api/trpc'
+import { gamesRouter } from '@/server/api/routers/game'
+import { sessionRouter } from '@/server/api/routers/session'
 
 export const appRouter = createTRPCRouter({
   ok: publicProcedure.query(() => {
     return { ok: true }
   }),
+  games: gamesRouter,
+  session: sessionRouter,
 })
 
 export type AppRouter = typeof appRouter

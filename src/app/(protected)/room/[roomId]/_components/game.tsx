533ffe53544364c9fa47b662ce90cfa473be7541
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-29 11:31:59 -0500

    FEAT: add game view component 🎨

---
+++ src/app/(protected)/room/[roomId]/_components/game.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { api } from '@/trpc/react'
+import { useParams } from 'next/navigation'
+import { GameInfo } from '@/app/(protected)/room/[roomId]/_components/game-info'
+import { Board } from '@/app/(protected)/room/[roomId]/_components/board'
+import { CopyGameLink } from '@/components/copy-game-link'
+
+export function Game() {
+  const { roomId } = useParams<{ roomId: string }>()
+
+  const [gameInfo] = api.games.gameByRoom.useSuspenseQuery(
+    { roomId },
+    {
+      refetchInterval: 1500,
+    }
+  )
+
+  if (!gameInfo) {
+    return <h3>Loading...</h3>
+  }
+
+  if (gameInfo.status === 'waiting') {
+    return (
+      <div className='space-y-8'>
+        <GameInfo />
+
+        <div className='space-y-8'>
+          <h2>Waiting the players...</h2>
+          <CopyGameLink />
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className='space-y-8'>
+      <GameInfo />
+      <Board />
+    </div>
+  )
+}

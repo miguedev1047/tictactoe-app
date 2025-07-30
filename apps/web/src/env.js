7cb653fd398876c27072f07a0a890fd4909f082c
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-29 21:01:58 -0500

    FEAT: add new public env ⚒️

--- apps/web/src/env.js
+++ apps/web/src/env.js
@@ -23,7 +23,7 @@ export const env = createEnv({
   },
 
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_PROD_URL: z.string(),
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
@@ -43,7 +43,7 @@ export const env = createEnv({
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    NEXT_PUBLIC_PROD_URL: process.env.NEXT_PUBLIC_PROD_URL,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,

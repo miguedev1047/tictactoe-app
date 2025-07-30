ed20039d3d6e2a5df3c4bd6c65bb4cadb4e58c7f
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-29 21:01:39 -0500

    FIX: copy link function ⚒️

--- apps/web/src/components/copy-game-link.tsx
+++ apps/web/src/components/copy-game-link.tsx
@@ -3,13 +3,15 @@
 import { toast } from '@/components/ui/8bit/toast'
 import { Button } from '@/components/ui/8bit/button'
 import { usePathname } from 'next/navigation'
+import { env } from '@/env'
 
 export function CopyGameLink() {
   const pathname = usePathname()
 
   const handleCopyLink = async () => {
     try {
-      await navigator.clipboard.writeText(pathname)
+      const URL = `${env.NEXT_PUBLIC_PROD_URL}${pathname}`
+      await navigator.clipboard.writeText(URL)
       toast('Link copied!')
     } catch {
       toast('An ocurred to copy link')
